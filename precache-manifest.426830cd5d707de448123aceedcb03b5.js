ed5cb7b200b2e49d12c0759d4fedf1a7ee861dc8
Souvik50 <duttasouvik916@yahoo.in>
2020-07-08 13:24:40 +0530

    Updates

---
+++ precache-manifest.426830cd5d707de448123aceedcb03b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ddb804a7cc38b9c05d5dd9e149ef65e",
+    "url": "/monsters2/index.html"
+  },
+  {
+    "revision": "91a3f5dca27dde43f3c9",
+    "url": "/monsters2/static/css/main.daa2a9fb.chunk.css"
+  },
+  {
+    "revision": "1b62d5c56410c7498739",
+    "url": "/monsters2/static/js/2.422b15d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters2/static/js/2.422b15d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91a3f5dca27dde43f3c9",
+    "url": "/monsters2/static/js/main.f6fcd0ee.chunk.js"
+  },
+  {
+    "revision": "e7269e7f85a93c7a08e4",
+    "url": "/monsters2/static/js/runtime-main.7bcfc80a.js"
+  }
+]);
